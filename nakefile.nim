c4b7e0047c1b0342c42319d0aec919c0ab2fd822
KeMeGe <chaoskmg@gmail.com>
2015-03-16 12:59:07 +0800

    Update nakefile with tasks to build logger and server separately for debugging

--- nakefile.nim
+++ nakefile.nim
@@ -1,12 +1,23 @@
 import nake
 
 const
-  exeName = @["logger"]
+  exeName = @["logger", "server"]
 
-task "debug", "Build for debugging":
+task "debug", "Build everything for debugging":
   for exe in exeName:
     shell(nimExe, "c", "-d:debug", "--out:../" & exe, "src/" & exe)
 
-task "release", "Build for release":
+task "release", "Build everything for release":
   for exe in exeName:
     shell(nimExe, "c", "-d:release", "--out:../" & exe, "src/" & exe)
+
+task "logger", "Build logger for debugging":
+  let exe = "logger"
+  shell(nimExe, "c", "-d:debug", "--threads:on", "--out:../" & exe, "src/" & exe)
+
+task "server", "Build server for debugging":
+  let exe = "server"
+  shell(nimExe, "c", "-d:debug", "--out:../" & exe, "src/" & exe)
+
+task defaultTask, "lists all tasks":
+  listTasks()
