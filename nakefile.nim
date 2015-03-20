61cefba7568a19fa42afa85918c50acced8ca7d6
KeMeGe <chaoskmg@gmail.com>
2015-03-20 19:01:03 +0800

    Add some icons by Freepik; update nakefile for Windows to include these icons

--- nakefile.nim
+++ nakefile.nim
@@ -5,19 +5,27 @@ const
 
 task "debug", "Build everything for debugging":
   for exe in exeName:
-    shell(nimExe, "c", "-d:debug", "--out:../" & exe, "src/" & exe)
+    when defined(windows):
+      var extra = "--passL:resources/" & exe & ".res"
+    shell(nimExe, "c", "-d:debug", "--out:../" & exe, extra, "src/" & exe)
 
 task "release", "Build everything for release":
   for exe in exeName:
-    shell(nimExe, "c", "-d:release", "--out:../" & exe, "src/" & exe)
+    when defined(windows):
+      var extra = "--passL:resources/" & exe & ".res"
+    shell(nimExe, "c", "-d:release", "--out:../" & exe, extra, "src/" & exe)
 
 task "logger", "Build logger for debugging":
   let exe = "logger"
-  shell(nimExe, "c", "-d:debug", "--threads:on", "--out:../" & exe, "src/" & exe)
+  when defined(windows):
+    var extra = "--passL:resources/" & exe & ".res"
+  shell(nimExe, "c", "-d:debug", "--threads:on", "--out:../" & exe, extra, "src/" & exe)
 
 task "server", "Build server for debugging":
   let exe = "server"
-  shell(nimExe, "c", "-d:debug", "--out:../" & exe, "src/" & exe)
+  when defined(windows):
+    var extra = "--passL:resources/" & exe & ".res"
+  shell(nimExe, "c", "-d:debug", "--out:../" & exe, extra, "src/" & exe)
 
 task defaultTask, "lists all tasks":
   listTasks()
